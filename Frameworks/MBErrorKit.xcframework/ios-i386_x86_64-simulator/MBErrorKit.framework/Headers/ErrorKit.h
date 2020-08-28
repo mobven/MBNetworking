2984e5848da0daa124075d700377f4f4cb8d6d56
Rashidium <rasid.ramazanov@mobven.com>
2020-08-28 16:12:12 +0300

    Embedded MobKitCore framework. Fastlane for distributing xcframework.

---
+++ Frameworks/MBErrorKit.xcframework/ios-i386_x86_64-simulator/MBErrorKit.framework/Headers/ErrorKit.h
@@ -0,0 +1,19 @@
+//
+//  ErrorKit.h
+//  ErrorKit
+//
+//  Created by Rasid Ramazanov on 08.06.2020.
+//  Copyright © 2020 Rasid Ramazanov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ErrorKit.
+FOUNDATION_EXPORT double ErrorKitVersionNumber;
+
+//! Project version string for ErrorKit.
+FOUNDATION_EXPORT const unsigned char ErrorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ErrorKit/PublicHeader.h>
+
+
