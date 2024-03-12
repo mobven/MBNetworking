a4b07aa8fbfb87fbe4261daa2611c8f09405e8ba
Hasan Güler <hasan.guler@mobven.com>
2024-03-12 14:21:17 +0300

    Created NetworkLogMonitoringDelegate, added setter in NetworkableConfigs

---
+++ Sources/MBNetworking/NetworkLogMonitoringDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkLogMonitoring.swift
+//  MBNetworking
+//
+//  Created by Hasan Güler on 12.03.2024.
+//
+
+import Foundation
+
+public protocol NetworkLogMonitoringDelegate {
+    func logTaskCreated(task: URLSessionTask)
+    func logDataTask(dataTask: URLSessionDataTask, didReceive data: Data)
+    func logTask(task: URLSessionTask, didFinishCollecting metrics: URLSessionTaskMetrics)
+    func logTask(task: URLSessionTask, didCompleteWithError error: Error?)
+}
